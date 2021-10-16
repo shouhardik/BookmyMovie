9985e043a700692a50cb644efd4a5e20e5bb926b
Shouhardik Saha <shouhardiksaha@gmail.com>
2021-10-17 00:44:57 +0530

    movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReactPayPal from './Components/PayPal/ReactPayPal';
 
-function App() {
+export default function App() {
+  const [checkout, setCheckout] = React.useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {(checkout === true) 
+          ? <div className="payment-div">
+            <ReactPayPal />
+          </div> 
+
+          :<div>
+            <h1>React-PayPal</h1>
+            <button onClick={() => {setCheckout(true)}} className="checkout-button">Checkout</button>
+          </div>
+        }
       </header>
     </div>
   );
 }
-
-export default App;
